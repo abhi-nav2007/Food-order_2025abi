23d0d6f4d5e46661aebc7d07dc01b67a9e55a137
abhi-nav2007 <kln2026abhinav.s.k@gmail.com>
2025-04-30 10:45:46 +0530

    Create menu.php
    C:\xampp\htdocs\food-order\admin\partials\menu.php

---
+++ menu.php
@@ -0,0 +1,32 @@
+<?php
+include 'C:/xampp/htdocs/food-order/admin/config/constants.php';
+?>
+
+
+<html>
+    <head>
+        <title>Food Order Website - Home Page</title>
+        <link rel="stylesheet" href="../css/admin.css">
+        <style>
+            .tbl-30{
+                width: 30%;
+            }
+        </style>
+    </head>
+    <body>
+<!-- < ! menu Section Starts -->
+<div class="menu text-center">
+    <div class="warpper">
+<ul>
+    <!-- <li><a href="index.php">Home</a></li> -->
+    <li><a href="manage-admin.php">Admin</a></li>
+    <li><a href="manage-category.php">Category</a></li>
+    <li><a href="manage-food.php">Food</a></li>
+    <!-- <li><a href="manage-order.php">Order</a></li> -->
+    <li><a href="logout.php">Logout</a></li>
+</ul>
+</div>
+</div>
+</body>
+</html> 
+
